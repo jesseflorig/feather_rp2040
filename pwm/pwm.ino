76152ec2176845873f1ac23fbd3bc2aca2d6edb5
Jesse Florig <jesseflorig@gmail.com>
2025-07-13 04:52:53 -0400

    cleaned up logic

--- pwm/pwm.ino
+++ pwm/pwm.ino
@@ -1,36 +1,56 @@
 #include <math.h>
 
 // Pin Setup
-const int pwmPinA = 6;  // Use a PWM-capable pin
+const int pwmPin = 6;  // Use a PWM-capable pin
 
 const int resolution = 16;      // RP2040 is 16-bit
 const uint16_t pwmMax = 65535;  // 2^16
 
-// Gamma correction factor
-const float gammaCorrection = 2.2;
-
-// Number of steps in the fade
+// Transition config
 const int steps = 256;
+const int stepDelay = 10;
+
+// Easing function that starts slow, speeds up in the middle, and ends slow
+float easeInOutQuad(float idx) {
+  return (idx < 0.5) ? 2.0 * idx * idx : -1.0 + (4.0 - 2.0 * idx) * idx;
+}
+
+void setPWM(float progress, int startVal, int targetVal) {
+  float eased = easeInOutQuad(progress);
+  float maxVal = max(startVal, targetVal) / 100;
+  float minVal = min(startVal, targetVal) / 100;
+  float percent = minVal + (eased * maxVal);
+  int pwmVal = (int)(percent * pwmMax);
+  analogWrite(pwmPin, pwmVal);
+  delay(stepDelay);
+}
+
+void fade(int startVal, int targetVal){
+  if (startVal == targetVal) return;
+  
+  if (startVal < targetVal) {  // Fade up
+    for (int i = 0; i <= steps; i++) {
+      float progress = (float)i / (float)steps;
+      setPWM(progress, startVal, targetVal);
+    }
+  }
+  else {                          // Fade down
+    for (int i = steps; i >= 0; i--) {
+      float progress = (float)i / (float)steps;
+      setPWM(progress, startVal, targetVal);
+    }
+  }
+}
 
 void setup() {
   analogWriteResolution(resolution);
-  pinMode(pwmPinA, OUTPUT);
+  pinMode(pwmPin, OUTPUT);
 }
 
+// TODO: Get percentages less than 100 working :/
 void loop() {
-  // Fade up
-  for (int i = 1; i <= steps; i++) {
-    float brightness = pow((float)i / steps, gammaCorrection);  
-    uint16_t pwmValue = (uint16_t)(brightness * pwmMax);
-    analogWrite(pwmPinA, pwmValue);
-    delay(10);
-  }
-
-  // Fade down
-  for (int i = steps; i >= 1; i--) {
-    float brightness = pow((float)i / steps, gammaCorrection);  
-    uint16_t pwmValue = (uint16_t)(brightness * pwmMax);
-    analogWrite(pwmPinA, pwmValue);
-    delay(10);
-  }
+  fade(0, 100);  // Turn on
+  delay(1000);   // Wait
+  fade(100, 0);  // Turn off
+  delay(1000);   // Wait
 }
